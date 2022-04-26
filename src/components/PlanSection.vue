9058ae5116bd0a1e0f7041d9e8de6154ade31e08
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-04-26 19:07:25 +0200

    completed IntroductionSection in main

---
+++ src/components/PlanSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="plan_container">ciao</div>
+</template>
+
+<script>
+export default {
+  name: "PlanSection",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/variables.scss";
+
+#plan_container {
+  background-color: $color-Gray-Nurse;
+}
+</style>
