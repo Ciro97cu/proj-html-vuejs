2f5f702b49a5127e81405cf00a9b00b7b482b0b5
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-04-26 15:58:31 +0200

    ended header

--- src/components/JumboAvada.vue
+++ src/components/JumboAvada.vue
@@ -13,54 +13,20 @@
             <font-awesome-icon icon="fa-solid fa-arrow-right" class="ps-2" />
           </button>
         </div>
-        <div class="offset-sm-2 col-sm-4 bg-white rounded-3 p-4">
-          <div class="d-flex flex-column align-items-center gap-3">
-            <div class="wrapper_img">
-              <img
-                class="rounded-circle w-100"
-                src="@/assets/img/marketing-consultant-expert.jpg"
-                alt="Richard Madsen"
-              />
-            </div>
-            <h4>Richard Madsen</h4>
-            <p>Marketing Consultant Expert</p>
-            <div class="d-inline-block pe-3 telephone">
-              <font-awesome-icon icon="fa-solid fa-phone" class="pe-2" />
-              (555) 802-1234
-            </div>
-            <form class="w-100">
-              <input
-                class="d-block w-100 mb-3 p-2"
-                type="text"
-                placeholder="Your Name*"
-              />
-              <input
-                class="d-block w-100 mb-3 p-2"
-                type="email"
-                placeholder="Your Email*"
-              />
-              <input
-                class="d-block w-100 mb-3 p-2"
-                type="tel"
-                placeholder="Your Phone Number*"
-              />
-              <input
-                class="d-block w-100 submit border-0 btn p-3"
-                type="submit"
-                value="Get a Callback"
-              />
-            </form>
-            <p class="credits">By submitting my data I agree to be contacted</p>
-          </div>
-        </div>
+        <MarketingExpertCard />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import MarketingExpertCard from "./MarketingExpertCard.vue";
+
 export default {
   name: "JumboAvada",
+  components: {
+    MarketingExpertCard,
+  },
 };
 </script>
 
@@ -73,7 +39,7 @@ export default {
   background-size: cover;
 
   .row {
-    min-height: 700px;
+    min-height: 600px;
     position: relative;
 
     .col-sm-6 {
@@ -88,41 +54,6 @@ export default {
 
       button {
         @include button(white, black, transparent, white);
-      }
-    }
-
-    .col-sm-4 {
-      position: absolute;
-      bottom: -10%;
-      right: 0;
-      box-shadow: 0px 10px 21px 10px rgba(0, 0, 0, 0.2);
-      .wrapper_img {
-        width: 70px;
-      }
-      h4 {
-        font-weight: 500;
-      }
-      p {
-        color: $color-Gunsmoke;
-      }
-      .telephone {
-        color: $color-Ecstasy;
-        font-weight: 500;
-      }
-      input {
-        border: 2px solid $color-Gray-Nurse;
-        border-radius: 5px;
-        &::placeholder {
-          color: $color-Tundora;
-        }
-      }
-
-      .submit {
-        @include button($color-Ecstasy, white, $color-Ecstasy-Op, black);
-      }
-      p:last-child {
-        font-size: 12px;
-        font-weight: 500;
       }
     }
   }
---
+++ src/components/MarketingExpertCard.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <div class="offset-sm-2 col-sm-4 bg-white rounded-3 p-5">
+      <div class="d-flex flex-column align-items-center gap-3">
+        <div class="wrapper_img">
+          <img
+            class="rounded-circle w-100"
+            src="@/assets/img/marketing-consultant-expert.jpg"
+            alt="Richard Madsen"
+          />
+        </div>
+        <h4>Richard Madsen</h4>
+        <p>Marketing Consultant Expert</p>
+        <div class="d-inline-block pe-3 telephone">
+          <font-awesome-icon icon="fa-solid fa-phone" class="pe-2" />
+          (555) 802-1234
+        </div>
+        <form class="w-100">
+          <input
+            class="d-block w-100 mb-3 p-2"
+            type="text"
+            placeholder="Your Name*"
+          />
+          <input
+            class="d-block w-100 mb-3 p-2"
+            type="email"
+            placeholder="Your Email*"
+          />
+          <input
+            class="d-block w-100 mb-3 p-2"
+            type="tel"
+            placeholder="Your Phone Number*"
+          />
+          <input
+            class="d-block w-100 submit border-0 btn p-3"
+            type="submit"
+            value="Get a Callback"
+          />
+        </form>
+        <p class="credits">By submitting my data I agree to be contacted</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MarketingExpertCard",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/variables.scss";
+@import "../assets/style/mixin.scss";
+
+.col-sm-4 {
+  position: absolute;
+  bottom: -10%;
+  right: 0;
+  box-shadow: 0px 10px 21px 10px rgba(0, 0, 0, 0.2);
+  .wrapper_img {
+    width: 100px;
+  }
+  h4 {
+    font-weight: 500;
+  }
+  p {
+    color: $color-Gunsmoke;
+  }
+  .telephone {
+    color: $color-Ecstasy;
+    font-weight: 500;
+  }
+  input {
+    border: 2px solid $color-Gray-Nurse;
+    border-radius: 5px;
+    &::placeholder {
+      color: $color-Tundora;
+    }
+  }
+
+  .submit {
+    @include button($color-Ecstasy, white, $color-Ecstasy-Op, black);
+  }
+  p:last-child {
+    font-size: 12px;
+    font-weight: 500;
+  }
+}
+</style>
