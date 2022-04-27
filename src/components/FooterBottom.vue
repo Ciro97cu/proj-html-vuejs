aeaec1009df7d4a340cf45ba78ed32ab29edce7d
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-04-27 15:01:06 +0200

    completed credits

--- src/components/FooterBottom.vue
+++ src/components/FooterBottom.vue
@@ -1,5 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <div class="container-sm pb-5">
+      <div class="row align-items-center pb-5">
+        <div class="col-sm-6">
+          <em
+            >&copy; 2020 <span class="mini_bullet">•</span> Avada Consultant
+            <span class="mini_bullet">•</span> Powered by WordPress</em
+          >
+        </div>
+        <div class="col-sm-3">
+          <em>Call Us (555) 802-1234</em>
+        </div>
+        <div class="col-sm-3 text-end">
+          <em>info@yourcompany.com</em>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +26,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+em {
+  font-size: 14px;
+
+  .mini_bullet {
+    font-size: 20px;
+  }
+}
 </style>
